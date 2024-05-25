60dc5e848fbcf3da1bed0f6baa7250908b8d370d
Jobet Casquejo <jobetcasquejo05@gmail.com>
2024-05-25 04:50:19 +0000

    feat: Added re_path import and added new API endpoints for admin and announcement

--- inventory/inventory/urls.py
+++ inventory/inventory/urls.py
@@ -19,9 +19,10 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.i18n import JavaScriptCatalog
 from django.views.static import serve
+from attendance import views
 
 urlpatterns = i18n_patterns(
     path("en/jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
@@ -29,6 +30,18 @@ urlpatterns = i18n_patterns(
     path("en/filer/", include("filer.urls")),
     path("", include("cms.urls")),
     path("en/media/", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^api/admin/$", views.admin_list),
+    re_path(r"^api/admin/([0-9])$", views.admin_details),
+    re_path(r"^api/announcement/$", views.announcement_list),
+    re_path(r"^api/announcement/([0-9])$", views.announcement_details),
+    re_path(r"^api/employee/$", views.employee_list),
+    re_path(r"^api/employee/([0-9])$", views.employee_details),
+    re_path(r"^api/event/$", views.event_list),
+    re_path(r"^api/event/([0-9])$", views.event_details),
+    re_path(r"^api/mobile_scan_user/$", views.mobile_scan_user_list),
+    re_path(r"^api/mobile_scan_user/([0-9])$", views.mobile_scan_user_details),
+    re_path(r"^api/static_time/$", views.statictime_list),
+    re_path(r"^api/static_time/([0-9])$", views.statictime_details),
 )
 
 
