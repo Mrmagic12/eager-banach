5d0908f343095418f33ed3753359b3d294ca77a8
Jobet Casquejo <jobetcasquejo05@gmail.com>
2024-05-25 13:33:22 +0000

    style: Update file 'settings.py' to include BASE_DIR in DIRS list, update 'base.html' to fix typo in 'extend' tag and update 'home.html' to display yesterday's announcements instead of today's

--- ams/ams/settings.py
+++ ams/ams/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'ams.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'attendance/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
